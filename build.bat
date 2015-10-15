3ef9667f087dcd7d24f7780c5740172575e2b24b
David Sferruzza <david.sferruzza@gmail.com>
2015-10-15 12:30:55 +0200

    Change slides engine

--- build.bat
+++
@@ -1,2 +0,0 @@
-pandoc -t dzslides -s --css=styles.css talk-evaluation-paresseuse-en-js.md -o talk-evaluation-paresseuse-en-js.html
-pandoc -t dzslides -s --self-contained --css=styles.css talk-evaluation-paresseuse-en-js.md -o packed.html
